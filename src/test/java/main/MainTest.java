804e3135f556d44bcf9e2fcb198f6e94458ddab1
Suduki <olle.lenti@gmail.com>
2018-03-05 09:34:43 +0100

    Add MainTest for JUnit

---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,15 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		System.out.println("Hejsan Jorden!");
+		fail("Not yet implemented");
+	}
+
+}
